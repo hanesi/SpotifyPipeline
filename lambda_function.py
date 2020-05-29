79440d26581351295891ee895bc9e99e3ae92ccc
hanesi <ian_hanes@utexas.edu>
2020-05-29 16:58:43 -0400

    Added artist/genre info
    Added functionality to retrieve and store information about artists and genres

--- lambda_function.py
+++ lambda_function.py
@@ -37,16 +37,57 @@ def lambda_handler(event,context):
     sp = spotipy.Spotify(auth=token)
     results = sp.current_user_recently_played()
 
+    print('Extracting Artist Names')
     artist_list = []
     for i in results['items']:
         artist = i['track']['artists'][0]['name']
         artist_list.append(artist)
+
+    artistIDs = set([i['track']['artists'][0]['id'] for i in results['items']])
+
+    print('Generating Artist and Genre Tables')
+    artistInfo = []
+    genreMapper = []
+    for i in artistIDs:
+        artistDict = {}
+        genreDict = {}
+        rawInfo = sp.artist(i)
+        artistDict['artistID'] = rawInfo['uri']
+        artistDict['artistName'] = rawInfo['name']
+        artistDict['artistPopularity'] = rawInfo['popularity']
+        artistDict['artistFollowers'] = rawInfo['followers']['total']
+        artistInfo.append(artistDict)
+
+        ID = rawInfo['uri']
+        genres = rawInfo['genres']
+        for j in genres:
+            ok = {}
+            ok['ID'] = ID
+            ok['genre'] = j
+            genreMapper.append(ok)
+
+    print('Writing genre/artist Tables')
+    dfArtists = pd.DataFrame(artistInfo)
+    dfArtists = dfArtists[['artistID', 'artistName', 'artistPopularity', 'artistFollowers']]
+    dfGenreMapper = pd.DataFrame(genreMapper)
+    dfGenreMapper = dfGenreMapper[['ID', 'genre']]
+
+    dfArtists.to_sql(name = 'ArtistInfo', con = engine, schema = 'spotifydb1',if_exists='append',index=False)
+    dfGenreMapper.to_sql(name = 'GenreMapper', con = engine, schema = 'spotifydb1',if_exists='append',index=False)
 
     cursor = connection.cursor()
     cursor.execute("SELECT max(played_at) FROM spotifydb1.StreamingLog;")
     max_date = cursor.fetchall()
     max_date = max_date[0][0].strftime('%Y-%m-%d %H:%M:%S')
 
+    print('Dropping DUPs from Artist/Genre Tables')
+    cursor.execute("alter ignore table ArtistInfo add unique(artistID);")
+    cursor.execute("ALTER TABLE `spotifydb1`.`ArtistInfo` DROP INDEX `artistID`;")
+
+    cursor.execute("alter ignore table GenreMapper add unique(ID, genre);")
+    cursor.execute("ALTER TABLE `spotifydb1`.`GenreMapper` DROP INDEX `ID`;")
+
+    print('Creating StreamingLog Table')
     test = json_normalize(results['items'])
     test = test.drop(['track.available_markets','track.album.available_markets'],axis =1)
     test['artist'] = artist_list
@@ -59,6 +100,7 @@ def lambda_handler(event,context):
 
     dfUpload = dfUpload[dfUpload['played_at'] > max_date]
 
+    print('Uploading StreamingLog')
     if len(dfUpload) == 0:
         connection.close()
     else:
